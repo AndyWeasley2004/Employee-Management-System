950a9a6a7a410f5ce4ceea83481be20e855910ca
AndyWeasley2004 <110661816+AndyWeasley2004@users.noreply.github.com>
2023-07-26 11:53:16 -0700

    Add files via upload

---
+++ EMS-frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+import './App.css'
+import ListEmployeeComponent from './components/ListEmployeeComponent'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import EmployeeComponent from './components/EmployeeComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <BrowserRouter>
+     <HeaderComponent />
+
+      <Routes>
+        <Route path='/' element = { <ListEmployeeComponent />}></Route>
+        {/* // http://localhost:3000/employees */}
+        <Route path='/employees' element = { <ListEmployeeComponent />}></Route>
+        <Route path='/add-employee' element = { <EmployeeComponent />}></Route>
+        <Route path='/edit-employee/:id' element = { <EmployeeComponent /> }></Route>
+
+      </Routes>
+
+     <FooterComponent/>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ EMS-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All Rights Reserved 2023 by Rivendell</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
